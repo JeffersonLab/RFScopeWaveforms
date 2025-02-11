a3ba48a34f38171c802be599eb1669e285824622
Adam Carpenter <adamc@jlab.org>
2025-02-11 17:15:09 -0500

    Init commit for docsrc framework

---
+++ docsrc/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'rfscopedb'
+copyright = '2025, Adam Carpenter'
+author = 'Adam Carpenter'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    #'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+]
+
+autosummary_generate = True
+html_theme = "sphinx_rtd_theme"
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ['_static']
---
+++ docsrc/source/index.rst
@@ -0,0 +1,17 @@
+.. rfscopedb documentation master file, created by
+   sphinx-quickstart on Tue Feb 11 17:11:10 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+rfscopedb documentation
+=======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
